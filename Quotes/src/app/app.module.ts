952ef1e36e19279becbaff48f7dfab5c9b882055
1offmark <official1offmark@gmail.com>
2021-08-21 20:58:45 +0300

    added like & quote form components to app.module.ts

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { QuotesPipe } from './quotes.pipe';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { LikeComponent } from './like/like.component';
 
 @NgModule({
   declarations: [
