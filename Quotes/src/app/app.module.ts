27447fcf15137a4cc2c17f8eca03634b00505d74
1offmark <official1offmark@gmail.com>
2021-08-21 20:57:31 +0300

    added quote & quote detail components to app.module.ts

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesPipe } from './quotes.pipe';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
